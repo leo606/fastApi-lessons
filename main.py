b5c1db66629c7c3fc2b01b297c7ffbd00ae3516a
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-19 18:12:33 -0300

    parse param to int

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from ast import parse
+from ftplib import parse257
+from xml.dom.expatbuilder import parseString
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,9 +8,9 @@ app = FastAPI()
 
 @app.get('/')
 def index():
-  return {'data': {'name': 'elvis'}}
+    return {'data': {'name': 'elvis'}}
 
 
-@app.get('/about')
-def about():
-  return ['hello, this is about route']
+@app.get('/blog/{id}')
+def get(id):  # is as function param
+    return {'param': int(id)}
