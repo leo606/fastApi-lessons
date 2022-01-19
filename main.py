452c6cbda166a9bb5ae5e77d6a0ceb9ec88e0c92
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-19 17:47:17 -0300

    about route

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get('/')
 def index():
-  return 'hello world'
+  return {'data': {'name': 'elvis'}}
+
+
+@app.get('/about')
+def about():
+  return ['hello, this is about route']
