db35e33b72d6fed57cab9e211fb10c37fd7a3582
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-20 17:47:16 -0300

    blog not fount response

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,5 @@
-from turtle import title
-from urllib import request
-from fastapi import FastAPI, Depends
+from urllib import response
+from fastapi import FastAPI, Depends, Response, status, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -10,16 +9,30 @@ models.Base.metadata.create_all(engine)
 app = FastAPI()
 
 def get_db():
-    db = SessionLocal()
-    try:
-      yield db
-    finally:
-      db.close()
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
 
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create(req: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title = req.title, body = req.body)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
+  new_blog = models.Blog(title = req.title, body = req.body)
+  db.add(new_blog)
+  db.commit()
+  db.refresh(new_blog)
+  return new_blog
+
+@app.get('/blog')
+def all(db: Session = Depends(get_db)):
+  blogs = db.query(models.Blog).all()
+  return blogs
+
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+def byId(id: int, response: Response, db: Session = Depends(get_db)):
+  allBlogs = db.query(models.Blog).filter(models.Blog.id == id).first()
+
+  if not allBlogs:
+    response.status_code = status.HTTP_404_NOT_FOUND
+    return f'id {id} not found'
+  return allBlogs
