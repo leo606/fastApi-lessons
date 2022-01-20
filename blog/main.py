417af0a012fd549319424a6bab3d7882cb1d930b
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-20 17:15:29 -0300

    post blog post a blog in the db

--- blog/main.py
+++ blog/main.py
@@ -1,12 +1,25 @@
-from fastapi import FastAPI
+from turtle import title
+from urllib import request
+from fastapi import FastAPI, Depends
 from . import schemas, models
-from .database import engine
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 models.Base.metadata.create_all(engine)
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+      yield db
+    finally:
+      db.close()
 
 @app.post('/blog')
-def create(req: schemas.Blog):
-    return req
+def create(req: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title = req.title, body = req.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
