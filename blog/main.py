fd2631069b7d1c20df3f0cf5dfba555ac8cc308d
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-22 15:46:49 -0300

    delete and update a blog

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,5 @@
-from urllib import response
+from pyexpat import model
+from turtle import title
 from fastapi import FastAPI, Depends, Response, status, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
@@ -23,7 +24,27 @@ def create(req: schemas.Blog, db: Session = Depends(get_db)):
   db.refresh(new_blog)
   return new_blog
 
-@app.get('/blog')
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def remove(id, db: Session=Depends(get_db) ):
+  blog = db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session= False)
+  if not blog:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'blog {id} not found')
+  db.commit()
+  return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, req: schemas.Blog, db: Session=Depends(get_db)):
+  blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+  if not blog.first():
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+      detail=f"Blog with id {id} not found")
+
+  blog.update({"title": req.title, "body": req.body })
+  db.commit()
+  return 'updated'
+
+@app.get('/blog', status_code=status.HTTP_200_OK)
 def all(db: Session = Depends(get_db)):
   blogs = db.query(models.Blog).all()
   return blogs
