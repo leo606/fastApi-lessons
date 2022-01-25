e39caa6d5c6aaa2964dc3bd02ad7aa973a4e3d1d
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-25 13:44:35 -0300

    users model and schema

--- blog/main.py
+++ blog/main.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import FastAPI, Depends, Response, status, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
@@ -49,7 +50,7 @@ def update(id, req: schemas.Blog, db: Session = Depends(get_db)):
     return 'updated'
 
 
-@app.get('/blog', status_code=status.HTTP_200_OK)
+@app.get('/blog', status_code=status.HTTP_200_OK, response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
@@ -63,3 +64,7 @@ def byId(id: int, db: Session = Depends(get_db)):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f'blog {id} not found')
     return allBlogs
+
+@app.post('/user', status_code=status.HTTP_201_CREATED)
+def createUser(req: schemas.User, db: Session = Depends(get_db)):
+  return req
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,10 +1,20 @@
+from re import S
 from pydantic import BaseModel
+
+from blog.database import Base
 
 
 class Blog(BaseModel):
   title: str
   body: str
 
-class ShowBlog(Blog):
+class ShowBlog(BaseModel):
+  title: str
+  body: str
   class Config():
     orm_mode = True
+
+class User(BaseModel):
+  name: str
+  email: str
+  password: str
